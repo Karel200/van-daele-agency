8e098cc840b9509ad932d981f602ec50df8ada6d
van Daele Karel (s129128) <karel.v.daele200@gmail.com>
2022-10-27 17:03:55 +0200

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
+    <main>
       <Layout pageTitle="Welcome to Inghelbrecht Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="randomized unsplash image!"
+        src="../images/learn.png"
+      />
       </Layout>
+    </main>
   )
 }
 
