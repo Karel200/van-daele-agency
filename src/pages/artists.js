aced9da2e080a4553508c134de0fc0f12c0c47db
van Daele Karel (s129128) <karel.v.daele200@gmail.com>
2022-11-10 20:32:48 +0100

    Query data met GraphQL finished

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -1,10 +1,34 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
-const ArtistsPage = () => {
+
+const ArtistsPage = ({data: {allWpArtist: {edges}}}) => {
   return (
-    <Layout pageTitle="Artists of Van_Daele Agency">
-      <p>A list of artists will be displayed here.</p>
+    <Layout pageTitle="Artists of Inghelbrecht Agency">
+      {edges.map((item) => {
+        const artist = item.node.artistMeta;
+        return <p key={item.node.id}>{artist.firstName} {artist.lastName}</p>
+      })}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+   allWpArtist {
+    edges {
+      node {
+        id
+        artistMeta {
+          firstName
+          lastName
+          artistName
+        }
+      }
+    }
+  }
+}
+
+`
+
 export default ArtistsPage
